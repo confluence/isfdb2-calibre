7b59d875bf70d4fe225f18b9499d1476f172c784
Adrianna Pińska <adrianna.pinska@gmail.com>
2017-10-08 18:38:54 +0200

    some cleanup of worker

--- isfdb2-plugin/worker.py
+++ isfdb2-plugin/worker.py
@@ -27,71 +27,47 @@ class Worker(Thread): # Get details
     '''
 
     def __init__(self, url, result_queue, browser, log, relevance, plugin, timeout=20):
-        print("Ohai")
         Thread.__init__(self)
         self.daemon = True
-        self.url, self.result_queue = url, result_queue
-        self.log, self.timeout = log, timeout
-        self.relevance, self.plugin = relevance, plugin
+        self.url = url
+        self.result_queue = result_queue
+        self.log = log
+        self.timeout = timeout
+        self.relevance = relevance
+        self.plugin = plugin
         self.browser = browser.clone_browser()
-        self.cover_url = self.isfdb_id = self.isbn = None
 
     def run(self):
-        print("Ohai")
         try:
             self.get_details()
-        except:
-            self.log.exception('get_details failed for url: %r'%self.url)
+        except Exception as e:
+            self.log.exception('get_details failed in worker for url %r with error %r' % (self.url, e))
 
     def get_details(self):
-        try:
-            print('ISFDB url: %r'%self.url)
-            self.log.info('ISFDB url: %r'%self.url)
-            raw = self.browser.open_novisit(self.url, timeout=self.timeout).read().strip()
-        except Exception as e:
-            if callable(getattr(e, 'getcode', None)) and \
-                    e.getcode() == 404:
-                self.log.error('URL malformed: %r'%self.url)
-                return
-            attr = getattr(e, 'args', [None])
-            attr = attr if attr else [None]
-            if isinstance(attr[0], socket.timeout):
-                msg = 'ISFDB.org timed out. Try again later.'
-                self.log.error(msg)
-            else:
-                msg = 'Failed to make details query: %r'%self.url
-                self.log.exception(msg)
-            return
-
-        raw = raw.decode('cp1252', errors='replace')
-
-        if '<title>404 - ' in raw:
-            self.log.error('URL malformed: %r'%self.url)
-            return
-
-        try:
-            root = fromstring(clean_ascii_chars(raw))
-        except:
-            msg = 'Failed to parse ISFDB details page: %r'%self.url
-            self.log.exception(msg)
-            return
+        # TODO stripping out all exception handling; may put some back when we see what exceptions we get
+        self.log.info('Worker fetching ISFDB url: %r' % self.url)
+        
+        response = self.browser.open_novisit(self.url, timeout=self.timeout).read()
+        raw = response.decode('cp1252', errors='replace')
+        root = fromstring(clean_ascii_chars(raw))
 
         self.parse_details(root)
 
     def parse_details(self, root):
         isfdb_id = None
+        try:
+            isfdb_id = re.search('(\d+)$', self.url).groups(0)[0]
+        except:
+            self.log.exception('Error parsing ISFDB ID for url: %r' % self.url)
+            
         title = None
         authors = []
         isbn = None
         publisher = None
         pubdate = None
         
-        try:
-            isfdb_id = re.search('(\d+)$', self.url).groups(0)[0]
-        except:
-            self.log.exception('Error parsing ISFDB ID for url: %r' % self.url)
-        
         detail_nodes = root.xpath('//div[@id="content"]//td[@class="pubheader"]/ul/li')
+        
         if not detail_nodes:
             detail_nodes = root.xpath('//div[@id="content"]/div/ul/li') # no table (on records with no image)
 
@@ -119,82 +95,76 @@ class Worker(Thread): # Get details
                 elif section == 'Publisher':
                     publisher = detail_node.xpath('a')[0].text_content().strip()
                     #self.log.info(publisher)
-                elif section == 'Date':
+                elif section == 'Date':                    
                     pubdate = self._convert_date_text(detail_node[0].tail.strip())
                     #self.log.info(pubdate)
             except:
                 self.log.exception('Error parsing section %r for url: %r' % (section, self.url) )
 
         if not title or not authors or not isfdb_id:
-            self.log.error('Could not find title/authors/ISFDB ID for %r' % self.url)
-            self.log.error('ISFDB: %r Title: %r Authors: %r' % (isfdb_id, title,
-                authors))
+            self.log.error('Insufficient metadata found for %r' % self.url)
             return
 
         mi = Metadata(title, authors)
         mi.set_identifier('isfdb', isfdb_id)
-        self.isfdb_id = isfdb_id
+        # TODO: should we also extract old-timey pre-ISBN catalog identifiers from book records that have them?
 
         if isbn:
-            self.isbn = mi.isbn = isbn
+            mi.isbn = isbn
         if publisher:
             mi.publisher = publisher
         if pubdate:
             mi.pubdate = pubdate
             
         try:
-            mi.comments = self.parse_comments(root)
+            # TODO eliminate this option; always add the contents
+            default_append_contents = cfg.DEFAULT_STORE_VALUES[cfg.KEY_APPEND_CONTENTS]
+            append_contents = cfg.plugin_prefs[cfg.STORE_NAME].get(cfg.KEY_APPEND_CONTENTS, default_append_contents)
+
+            if append_contents:
+                contents_node = root.xpath('//div[@class="ContentBox"][2]/ul')
+                
+                if contents_node:
+                    contents = tostring(contents_node[0], method='html')            
+                    mi.comments = sanitize_comments_html(contents)
+
         except:
             self.log.exception('Error parsing comments for url: %r'%self.url)
 
         try:
-            self.cover_url = self.parse_cover(root)
+            img_src = root.xpath('//div[@id="content"]//table/tr[1]/td[1]/a/img/@src')
+            if img_src:
+                mi.cover_url = img_src[0]
+                self.plugin.cache_identifier_to_cover_url(self.isfdb_id, page_url)
         except:
             self.log.exception('Error parsing cover for url: %r'%self.url)
         
-        mi.has_cover = bool(self.cover_url)
-        mi.cover_url = self.cover_url # This is purely so we can run a test for it!!!
+        mi.has_cover = bool(mi.cover_url)
 
         mi.source_relevance = self.relevance
 
-        if self.isfdb_id:
-            if self.isbn:
-                self.plugin.cache_isbn_to_identifier(self.isbn, self.isfdb_id)
+        # TODO: do we actually want / need this?
+        if isfdb_id:
+            if isbn:
+                self.plugin.cache_isbn_to_identifier(isbn, isfdb_id)
 
         self.plugin.clean_downloaded_metadata(mi)
         self.result_queue.put(mi)
 
     def _convert_date_text(self, date_text):
-        # 2008-08-00
+        # We use this instead of strptime to handle dummy days and months
+        # E.g. 1965-00-00
         try:
-            year = int(date_text[0:4])
-            month = int(date_text[5:7])
+            year, month, day = [int(p) for p in date_text.split(":")]
+
             if month == 0:
                 month = 1
-            day = int(date_text[8:10])
+
             if day == 0:
                 day = 1
+            
             from calibre.utils.date import utc_tz
-            pubdate = datetime.datetime(year, month, day, tzinfo=utc_tz)
-            return pubdate
+            return datetime.datetime(year, month, day, tzinfo=utc_tz)
         except:
             return None # not a parseable date
 
-    def parse_comments(self, root):
-        default_append_contents = cfg.DEFAULT_STORE_VALUES[cfg.KEY_APPEND_CONTENTS]
-        append_contents = cfg.plugin_prefs[cfg.STORE_NAME].get(cfg.KEY_APPEND_CONTENTS, default_append_contents)
-        comments = ''
-        if append_contents:
-            contents_node = root.xpath('//div[@class="ContentBox"][2]/ul')
-            if contents_node:
-                contents = tostring(contents_node[0], method='html')
-                comments += contents
-        if comments:
-            return sanitize_comments_html(comments)
-
-    def parse_cover(self, root):
-        img_src = root.xpath('//div[@id="content"]//table/tr[1]/td[1]/a/img/@src')
-        if img_src:
-            page_url = img_src[0]
-            self.plugin.cache_identifier_to_cover_url(self.isfdb_id, page_url)
-            return page_url
